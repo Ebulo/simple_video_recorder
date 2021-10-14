8e50754f77eaa7be33bef63fedb977feb75786c5
fackid1 <thisfakeid.1234@gmail.com>
2021-10-14 21:54:36 +0530

    Added a time feature to the existing code

--- video_recorder.py
+++ video_recorder.py
@@ -1,6 +1,7 @@
 # This is a video recorder done with Open CV
 import cv2
 import datetime
+import time
 
 name = input("Please enter your name: ")
 cap = cv2.VideoCapture(0)
@@ -17,6 +18,7 @@ output = cv2.VideoWriter(vdo_name, video_fmt, 24, frame_size)
 
 input("Press enter to start recording")
 
+start_time = time.time()
 while True:
     _, frame = cap.read()
 
@@ -25,8 +27,12 @@ while True:
     cv2.imshow("Recorder", frame)
 
     if cv2.waitKey(0) == "q":
+        end_time = time.time()
+        print("Total length of the Video is: ", end_time-start_time)
         break
 
 output.release()
+end_time = time.time()
+print("Total length of the Video is: ", end_time-start_time)
 cap.release()
 cv2.destroyAllWindows()
